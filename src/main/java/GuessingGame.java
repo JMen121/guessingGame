c919d0d3ca061d1ad4aae6743092d1b36f067193
Janet_Mensah <janetmensah60@gmail.com>
2023-10-08 21:05:55 +0100

    Pseudo-code has been added before in order to understand procedures to be completed for the guessing game

---
+++ src/main/java/GuessingGame.java
@@ -0,0 +1,11 @@
+public class GuessingGame {
+    public static void main (String [] args){
+        //Guesing game//
+        //Takes user input- What number am I thinking of between 1 -20?//
+        //User inserts the number//
+        //Machine should output whether it is high or low//
+        //Machine should input congratulations if acheived//
+
+
+    }
+}
