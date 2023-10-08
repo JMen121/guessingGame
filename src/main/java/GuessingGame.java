d67bd2b6b8954ece544507d861e7ab34a7a905bd
Janet_Mensah <janetmensah60@gmail.com>
2023-10-08 22:06:18 +0100

    System output for string Hello User is working aswell as system output for the main question in the guessing game

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -2,19 +2,26 @@
 import java.util.Scanner;
 public class GuessingGame {
     public static void main (String [] args){
-        //Use Scanner class in order to achieve this//
-        Scanner sc = new Scanner (System.in);
-        String username;
-        //Welcome the user//
+        //Ask the user for their name//
         System.out.println("What is your name?");
+        //Use Scanner class in order to achieve this//
+        Scanner sc = new Scanner (System.in); //Scanner object has been Added//
+        String username = sc.nextLine(); //This has stringed the username and asked the machine to read the line the user writes
 
-        System.out.println("Hello" + "")
+       //Welcome the user//
+        System.out.println("Hello " + username);
+
         //Asks What number am I thinking of between 1 -30?//
         System.out.println("Am I thinking of a number between 1-30, What number am I thinking of?");
 
         //User inserts the number//
+       int userAnswer = Integer.parseInt(sc.nextLine());
+
+
         //Machine should output whether it is high or low//
-        //Machine should input congratulations if acheived//
+        if (userAnswer == 23);
+
+        //Machine should input congratulations if achieved//
 
 
     }
