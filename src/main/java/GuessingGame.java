5fd96b3407ffb4df3edaa1c411c6d28790fa4be9
Janet_Mensah <janetmensah60@gmail.com>
2023-10-08 22:33:21 +0100

    Ensure that if, else statments make sense

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,28 +1,39 @@
 
 import java.util.Scanner;
 public class GuessingGame {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         //Ask the user for their name//
         System.out.println("What is your name?");
         //Use Scanner class in order to achieve this//
-        Scanner sc = new Scanner (System.in); //Scanner object has been Added//
+        Scanner sc = new Scanner(System.in); //Scanner object has been Added//
         String username = sc.nextLine(); //This has stringed the username and asked the machine to read the line the user writes
 
-       //Welcome the user//
+        //Welcome the user//
         System.out.println("Hello " + username);
 
         //Asks What number am I thinking of between 1 -30?//
         System.out.println("Am I thinking of a number between 1-30, What number am I thinking of?");
 
         //User inserts the number//
-       int userAnswer = Integer.parseInt(sc.nextLine());
+        int userAnswer = Integer.parseInt(sc.nextLine());
+        int gameAnswer = 22;
 
-
-        //Machine should output whether it is high or low//
-        if (userAnswer == 23);
 
         //Machine should input congratulations if achieved//
+        if (userAnswer == 22) ;
+        {
+            System.out.println("Wohoo! You're a whizz at this!");
 
-
+            //Machine should output whether it is high or low//
+        } else if (userAnswer > gameAnswer) {
+            System.out.println("Woah Tiger, that's a higher than my number!");
+        } else if (userAnswer < gameAnswer) {
+            System.out.println("My number is higher than that!");
+        }
     }
 }
+
+
+//if useranswer is less 21,20 or 23,24 - almost there answer//
+
+
