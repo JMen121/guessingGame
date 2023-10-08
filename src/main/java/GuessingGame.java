a12263ba44bd0aaafbb5e8f9f8728bc4b603a221
Janet_Mensah <janetmensah60@gmail.com>
2023-10-08 21:30:51 +0100

    Ensured that the Scanner class has been added and pseudo code has been clarified

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,7 +1,17 @@
+
+import java.util.Scanner;
 public class GuessingGame {
     public static void main (String [] args){
-        //Guesing game//
-        //Takes user input- What number am I thinking of between 1 -20?//
+        //Use Scanner class in order to achieve this//
+        Scanner sc = new Scanner (System.in);
+        String username;
+        //Welcome the user//
+        System.out.println("What is your name?");
+
+        System.out.println("Hello" + "")
+        //Asks What number am I thinking of between 1 -30?//
+        System.out.println("Am I thinking of a number between 1-30, What number am I thinking of?");
+
         //User inserts the number//
         //Machine should output whether it is high or low//
         //Machine should input congratulations if acheived//
